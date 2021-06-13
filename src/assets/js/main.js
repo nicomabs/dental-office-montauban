4f76a9ee3f4e6d0103f6093f2d9fca05adfd0db8
nicomabs <nikkoboulay@gmail.com>
2021-06-13 20:11:47 +0200

    fade in message hero section

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -174,6 +174,31 @@
   }
 
   /**
+   * Welcome easin effect
+   */
+  // Wrap every letter in a span
+  var textWrapper = document.querySelector('.intro-fade-in');
+  textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+  anime.timeline({ loop: false })
+    .add({
+      targets: '.intro-fade-in .letter',
+      translateX: [40, 0],
+      translateZ: 0,
+      opacity: [0, 1],
+      easing: "easeOutExpo",
+      duration: 1800,
+      delay: (el, i) => 500 + 30 * i
+    }).add({
+      targets: '.intro-fade-in',
+      translateX: [0, -30],
+      opacity: [1, 0],
+      easing: "easeInExpo",
+      duration: 900,
+      delay: (el, i) => 100 + 30 * i
+    });
+
+  /**
    * Testimonials slider
    */
   new Swiper('.testimonials-slider', {
