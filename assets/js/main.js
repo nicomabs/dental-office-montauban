9a5931797c1c42314895a5f3463ee3b104a3b16b
nicomabs <nikkoboulay@gmail.com>
2021-07-11 14:33:21 +0200

    Updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -179,13 +179,6 @@
   }
 
   /**
-   * Initiate gallery lightbox 
-   */
-  const galleryLightbox = GLightbox({
-    selector: '.gallery-lightbox'
-  });
-
-  /**
    * Animation on scroll
    */
   window.addEventListener('load', () => {
