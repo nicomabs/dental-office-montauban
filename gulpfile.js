64ae1907192b7ea33ceb08e2947a8a6a776fd102
nicomabs <nikkoboulay@gmail.com>
2021-07-11 16:19:46 +0200

    install gulp minify images and gulp minify css(wip)

--- gulpfile.js
+++ gulpfile.js
@@ -8,9 +8,11 @@ const del = require('del');
 const gulp = require('gulp');
 const mergeStream = require('merge-stream');
 const nunjucks = require('gulp-nunjucks');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 const inject = require('gulp-inject-string');
 const vendors = require('./vendors.json');
+const imagemin = require('gulp-imagemin');
+const cleanCSS = require('gulp-clean-css');
 
 /**
  * Set the destination/production directory
@@ -28,34 +30,43 @@ function clean() {
 // Task: Copy Files
 function copyFiles() {
   const assetsFolder = gulp.src(['./src/assets/**/*', '!./src/assets/js/main.js'])
-    .pipe(gulp.dest(distDir+'assets/'))
+    .pipe(gulp.dest(distDir + 'assets/'))
     .pipe(browserSync.stream());
 
   const formsFolder = gulp.src('./src/forms/**/*')
-  .pipe(gulp.dest(distDir+'forms/'));
+    .pipe(gulp.dest(distDir + 'forms/'));
 
   return mergeStream(assetsFolder, formsFolder);
 }
 
+// Task: Minify images
+
+function minifyIMG() {
+  return gulp.src('src/assets/img/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/assets/img'))
+};
+
+
 // Task: Compile HTML
 function compileHTML() {
 
-  let css_links ='';
+  let css_links = '';
   let js_links = '';
   for (let vendor in vendors) {
-    if(vendors[vendor].css_link !== undefined) {
+    if (vendors[vendor].css_link !== undefined) {
       css_links += '<link href="' + vendors[vendor].css_link + '" rel="stylesheet">';
     }
-    if(vendors[vendor].js_link !== undefined) {
+    if (vendors[vendor].js_link !== undefined) {
       js_links += '<script src="' + vendors[vendor].js_link + '"></script>';
     }
   }
 
   return gulp.src(['./src/html/*.html'])
     .pipe(nunjucks.compile())
-    .pipe( inject.replace('<!-- Vendor CSS Files -->', '<!-- Vendor CSS Files -->' + css_links) )
-    .pipe( inject.replace('<!-- Vendor JS Files -->', '<!-- Vendor JS Files -->' + js_links) )
-    .pipe(beautify.html({ indent_size: 2, max_preserve_newlines: 1}))
+    .pipe(inject.replace('<!-- Vendor CSS Files -->', '<!-- Vendor CSS Files -->' + css_links))
+    .pipe(inject.replace('<!-- Vendor JS Files -->', '<!-- Vendor JS Files -->' + js_links))
+    .pipe(beautify.html({ indent_size: 2, max_preserve_newlines: 1 }))
     .pipe(gulp.dest(distDir))
     .pipe(browserSync.stream());
 }
@@ -70,15 +81,18 @@ function compileSCSS() {
     .pipe(autoprefixer({
       cascade: false
     }))
-    .pipe(gulp.dest(distDir+'assets/css'))
+    .pipe(gulp.dest(distDir + 'assets/css'))
     .pipe(browserSync.stream());
 }
 
+// Task: Minify CSS
+
+
 // Task: Compile JS
 function compileJS() {
-  return gulp.src( './src/assets/js/main.js')
-    .pipe(beautify.js({ indent_size: 2, max_preserve_newlines: 2}))
-    .pipe(gulp.dest(distDir+'assets/js'))
+  return gulp.src('./src/assets/js/main.js')
+    .pipe(beautify.js({ indent_size: 2, max_preserve_newlines: 2 }))
+    .pipe(gulp.dest(distDir + 'assets/js'))
     .pipe(browserSync.stream());
 }
 
@@ -87,11 +101,11 @@ function copyVendors() {
   let stream = mergeStream();
 
   for (let vendor in vendors) {
-    if(vendors[vendor].src) {
-      stream.add( gulp.src( vendors[vendor].src ).pipe( gulp.dest( distDir + vendors[vendor].dest ) ) );
-    } else if(vendors[vendor].srcs) { 
+    if (vendors[vendor].src) {
+      stream.add(gulp.src(vendors[vendor].src).pipe(gulp.dest(distDir + vendors[vendor].dest)));
+    } else if (vendors[vendor].srcs) {
       for (let multivendor in vendors[vendor].srcs) {
-        stream.add( gulp.src( vendors[vendor].srcs[multivendor].src ).pipe( gulp.dest( distDir + vendors[vendor].srcs[multivendor].dest ) ) );
+        stream.add(gulp.src(vendors[vendor].srcs[multivendor].src).pipe(gulp.dest(distDir + vendors[vendor].srcs[multivendor].dest)));
       }
     }
   }
@@ -116,11 +130,12 @@ function watchFiles() {
   gulp.watch('./src/html/**/*.html', compileHTML);
   gulp.watch(['./src/assets/**/*', '!./src/assets/js/main.js'], copyFiles);
   gulp.watch('./src/assets/js/main.js', compileJS);
+  gulp.watch('./src/assets/js/img/**/*', minifyIMG);
 }
 
 // Export tasks
-const dist = gulp.series(clean, gulp.parallel(copyFiles, compileHTML, compileSCSS, compileJS, copyVendors) );
+const dist = gulp.series(clean, gulp.parallel(copyFiles, compileHTML, compileSCSS, compileJS, minifyIMG, copyVendors));
 
 exports.watch = gulp.series(dist, watchFiles);
-exports.start = gulp.series(dist, gulp.parallel(watchFiles, initBrowserSync) );
+exports.start = gulp.series(dist, gulp.parallel(watchFiles, initBrowserSync));
 exports.default = dist;
